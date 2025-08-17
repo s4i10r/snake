9a869e5854609ed92bbad155013a6d204fa9f08b
Mohamed Y <mohamedpk@hotmail.de>
2025-08-17 13:38:24 +0200

    added some basic directioning

--- console_snake.py
+++ console_snake.py
@@ -2,13 +2,14 @@
 
 import os
 import time
+import msvcrt   # Windows specific
 
 # CONTANTS
 WIDTH = 30
 HEIGHT = 10
 
 running = True
-
+direction = "w"
 
 # this list contains the snakes position
 # also start position
@@ -25,15 +26,36 @@ def gen_field(WIDTH: int, HEIGHT: int) -> list:
 
     for i in range(HEIGHT):
         if i == 0 or i == HEIGHT - 1:
-            field.append(["*" for i in range(WIDTH)])
+            field.append(["-" for i in range(WIDTH)])
         else:
-            row = ["*"] + [" " for i in range(WIDTH-2)] + ["*"]
+            row = ["|"] + [" " for i in range(WIDTH-2)] + ["|"]
             field.append(row)
 
     return field
 
 field = gen_field(WIDTH, HEIGHT)
 
+
+def calc_snake(snake: list, direction: str, width: int, height: int)-> list:
+    """
+    calculates the positioning of the snake based on
+    the current snake + direction
+    """
+    del snake[0]
+
+    match direction:
+        # wasd - top/left/down/right
+        case "d":
+            # take the farthest position and extend it to the right
+            snake.append([snake[-1][0], (snake[-1][1]+1%width)])
+        case "a":
+            snake.append([snake[-1][0], (snake[-1][1]-1%width)])
+        case "w":
+            snake.append([(snake[-1][0]-1%height), snake[-1][1]])
+        case "s":
+            snake.append([(snake[-1][0]+1)%height, snake[-1][1]])
+    
+    return snake
 
 
 def repos_snake(snake: list, field: list) -> list:
@@ -43,7 +65,7 @@ def repos_snake(snake: list, field: list) -> list:
 
     # place snake
     for i in range(len(snake)):
-        field[snake[i][0]][snake[i][1]] = "O"
+        field[snake[i][0]][snake[i][1]] = "o"
 
     return field
 
@@ -67,8 +89,13 @@ if __name__ == "__main__":
             print("".join(row))
 
         # reposition snake
-        del snake[0]
-        # append the last farthest position + 1 and mod for travelling through the edge
-        snake.append([(snake[-1][0])%WIDTH, (snake[-1][1]+1)%WIDTH])
+        # del snake[0]
+        # # append the last farthest position + 1 and mod for travelling through the edge
+        # snake.append([(snake[-1][0])%WIDTH, (snake[-1][1]+1)%WIDTH])
 
-        time.sleep(0.5)
+        if msvcrt.kbhit():  # checks for keypress
+            direction = msvcrt.getch().decode("utf-8")
+            
+        snake = calc_snake(snake, direction, WIDTH, HEIGHT)
+
+        time.sleep(0.4)
