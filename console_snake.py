5f62c26beca2de86822551d8cbd8a8c9dc3e85ed
Mohamed Y <mohamedpk@hotmail.de>
2025-08-17 11:47:55 +0200

    packed some stuff into functions

--- console_snake.py
+++ console_snake.py
@@ -7,37 +7,71 @@ import time
 WIDTH = 30
 HEIGHT = 10
 
+running = True
+
+
 # this list contains the snakes position
-# start position
+# also start position
 snake = [[HEIGHT//2, WIDTH//2+i] for i in range(5)]
 
-# BASIC FIELD GENERATION AND DICT CREATION
-field = [["." for i in range(WIDTH)] for i in range(HEIGHT)]
+
+
+def gen_field(WIDTH: int, HEIGHT: int) -> list:
+    """
+    generates the snake field
+    """
+    # BASIC FIELD GENERATION
+    field = [["." for i in range(WIDTH)] for i in range(HEIGHT)]
+
+    return field
+
+field = gen_field(WIDTH, HEIGHT)
 
 
 
+# alternative field generation
+# alt_field = [[] for i in range(HEIGHT)] # TODO: hm
 
-for i in range(100):
-    # wipe screen for update
-    os.system("cls" if os.name == "nt" else "clear")
+# for i in range(WIDTH):
+#     alt_field[0].append("*")
+#     alt_field[-1].append("*")
 
-    # reset field
-    for row in field:
-        for i in range(len(row)):
-            row[i] = "."
 
-    
+def repos_snake(snake: list, field: list) -> list:
+    """
+    handles the reposition logic
+    """
+
     # place snake
     for i in range(len(snake)):
         field[snake[i][0]][snake[i][1]] = "O"
 
+    return field
 
-    for row in field:
-        print("".join(row))
 
-    # reposition snake
-    del snake[0]
-    # append the last farthest position + 1 and mod for travelling through the edge
-    snake.append([(snake[-1][0])%WIDTH, (snake[-1][1]+1)%WIDTH])
+# MAIN LOOP
 
-    time.sleep(0.5)
+if __name__ == "__main__":
+
+    while running:
+        # wipe screen for update
+        os.system("cls" if os.name == "nt" else "clear")
+
+        # reset field
+        for row in field:
+            for i in range(len(row)):
+                row[i] = "."
+
+        
+        field = repos_snake(snake, field)
+
+
+        for row in field:
+            print("".join(row))
+
+        # reposition snake
+        del snake[0]
+        # append the last farthest position + 1 and mod for travelling through the edge
+        snake.append([(snake[-1][0])%WIDTH, (snake[-1][1]+1)%WIDTH])
+
+        time.sleep(0.5)
